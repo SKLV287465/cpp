d40dbb72244e8bcdac2080afe2cd06720eda9abc
Joshua Shim <joshua.shim2004@gmail.com>
2026-03-06 17:58:18 +1100

    werw:

---
+++ connect_four/ConnectFour.cpp
@@ -0,0 +1,10 @@
+/**
+ * Connect four game, detect if four is done, diagonal and horizontal and vertical
+ * infinite plane left and right, zero is middle
+ * inputs, take turns
+ * Every turn detect is winner exists
+ * 
+ * O(1) insertion and sparse memory.
+ * simple game loop
+ * 
+ */
---
+++ elevators/Elevatos.cpp
@@ -0,0 +1,8 @@
+/**
+ * Building has N floors, starts at S
+ * M requests (request time, start floor, destination floor)
+ * 
+ * can move in 1 unit of time, stopping takes 1 additional time
+ * 
+ * create minimum  total time elevator
+ */
